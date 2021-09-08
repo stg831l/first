882240e5ef7d803105136d64adc399c1e2a9e733
stg831l <90320985+stg831l@users.noreply.github.com>
2021-09-09 02:38:18 +0800

    heatpde1

---
+++ heatpde.m
@@ -0,0 +1,27 @@
+clear
+a=1;
+dx=0.02;
+x=0:dx:1;
+dt=0.00005;
+t=0:dt:1;
+u=zeros(length(x),length(t));
+u(:,1)=sin(pi*x); 
+%隨意設置的初始條件
+m1=0+0.0*sin(t);
+m2=1-0.0*sin(t); 
+%隨意設置的邊界條件
+A=-2*eye(length(x))+diag(ones(1,length(x)-1),1)+diag(ones(1,length(x)-1),-1);
+%設定A矩陣
+for n=1:length(t)-1
+    u(:,n+1)=u(:,n)+a^2*dt/dx^2*A*u(:,n);
+    u(1,n+1)=m1(n+1);
+    u(end,n+1)=m2(n+1);
+    plot(x,u(:,n+1))
+    axis([x(1) x(end) 0 1])
+    getframe; 
+end
+% [T,X]=meshgrid(t,x);
+% surf(X,T,u);
+% shading interp %給圖上色
+% hold on
+
